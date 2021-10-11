6cbd6fde987490fffd81b48377cc8ddffc0edc25
decima <h.larget@gmail.com>
2021-10-11 10:24:00 +0200

    fix Sorting issues and Path issues

--- go.mod
+++ go.mod
@@ -6,7 +6,8 @@ require (
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver v1.5.0 // indirect
 	github.com/Masterminds/sprig v2.22.0+incompatible
-	github.com/gin-contrib/static v0.0.1 // indirect
+	github.com/fsnotify/fsnotify v1.4.7
+	github.com/gin-contrib/static v0.0.1
 	github.com/gin-gonic/gin v1.7.2
 	github.com/giorgisio/goav v0.1.0 // indirect
 	github.com/go-playground/validator/v10 v10.6.1 // indirect
@@ -19,7 +20,7 @@ require (
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/u2takey/ffmpeg-go v0.3.0 // indirect
+	github.com/u2takey/ffmpeg-go v0.3.0
 	github.com/ugorji/go v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 	golang.org/x/sys v0.0.0-20210521203332-0cec03c779c1 // indirect
---
+++ tools/templateFunctions.go
@@ -0,0 +1,81 @@
+package tools
+
+import (
+	"errors"
+	"fmt"
+	"github.com/Masterminds/sprig"
+	"html/template"
+	"net/url"
+	"reflect"
+	"strings"
+)
+
+func FuncMap() template.FuncMap {
+	fmap := sprig.FuncMap()
+	fmap["dict"] = Dict
+	fmap["hrsize"] = PrettyBytes
+	fmap["urlAddQuery"] = AddUrlArg
+	fmap["ftype"] = FileType
+	fmap["last"] = func(x int, a interface{}) bool {
+		return x == reflect.ValueOf(a).Len()-1
+	}
+	return fmap
+}
+
+func FileType(n string) string {
+	n = strings.ToLower(n)
+	types := map[string][]string{
+		"image": {"bmp", "gif", "jpg", "jpeg", "png", "tiff", "svg"},
+		"video": {"mp4", "mpeg", "avi", "mkv", "ogv", "webm", "3gp", "3g2", "mov"},
+		"audio": {"oga", "mid", "midi", "mp3", "opus", "wav", "weba", "aac"},
+		"doc":   {"md", "pdf", "html", "txt"},
+	}
+
+	for t, ext := range types {
+		for _, e := range ext {
+			if strings.HasSuffix(n, "."+e) {
+				return t
+			}
+		}
+	}
+	return "other"
+
+}
+
+func PrettyBytes(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
+}
+
+func Dict(values ...interface{}) (map[string]interface{}, error) {
+	if len(values)%2 != 0 {
+		return nil, errors.New("invalid dict call")
+	}
+	dict := make(map[string]interface{}, len(values)/2)
+	for i := 0; i < len(values); i += 2 {
+		key, ok := values[i].(string)
+		if !ok {
+			return nil, errors.New("dict keys must be strings")
+		}
+		dict[key] = values[i+1]
+	}
+	return dict, nil
+}
+
+func AddUrlArg(original *url.URL, k string, v string) *url.URL {
+	u, _ := url.Parse(original.String())
+	query := u.Query()
+	query.Del(k)
+	query.Add(k, v)
+	u.RawQuery = query.Encode()
+	return u
+}
